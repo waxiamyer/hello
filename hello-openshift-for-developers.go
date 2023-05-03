ef275d0b1ed735dabfe5f0274eee63532a5be72d
waxiamyer <58050316+waxiamyer@users.noreply.github.com>
2023-05-03 16:33:56 -0500

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World! Let's GO"
+		response = "Hello World! Openshift for Developers "
 	}
 
 	fmt.Fprintln(w, response)
